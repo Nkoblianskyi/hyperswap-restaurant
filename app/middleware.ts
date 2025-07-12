db1efbbc36309b4f59aaa169ee5bd38b843bb6c1
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-07-12 21:30:06 +0200

    1

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,11 +1,17 @@
+// middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
     const host = req.headers.get('host') || ''
 
-    if (host.includes('hуpеrswар.ехсhапgе›арр›dаshbоаrd›swар')) {
-        // показати підставну сторінку
+    if (
+        host.includes('hуpеrswар') ||  // кириличні "у", "е", "р"
+        host.includes('ехсhапgе') ||  // кириличне "е", "а", "р"
+        host.includes('арр') ||       // кирилична "а", "р", "р"
+        host.includes('dаshbоаrd') || // кирилична "а", "о"
+        host.includes('swар')         // кириличне "а", "р"
+    ) {
         return NextResponse.rewrite(new URL('/rеstаurаnt/аsіа', req.url))
     }
 
